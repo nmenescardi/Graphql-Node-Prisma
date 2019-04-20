5ea31151ee9a8547b6caf26f0fd362cc59e66045
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-04-20 13:08:39 -0400

    first commit

---
+++ graphql-basics/src/index.js
@@ -0,0 +1,54 @@
+import { GraphQLServer } from 'graphql-yoga';
+import uuid from 'uuid';
+
+// Type definitions (schema)
+const typeDefs = `
+type Query {
+
+  me: User!
+  post: Post!
+}
+
+type User {
+  id: ID!
+  name: String!
+  email: String!
+  age: Int
+}
+
+type Post {
+  id: ID!
+  title: String!
+  body: String!
+  published: Boolean!
+}
+
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    me() {
+      return {
+        id: '123098',
+        name: 'Mike',
+        email: 'mike@example.com'
+      };
+    },
+    post() {
+      return {
+        id: '092',
+        title: 'GraphQL 101',
+        body: '',
+        published: false
+      };
+    }
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => console.log('server up...'));
