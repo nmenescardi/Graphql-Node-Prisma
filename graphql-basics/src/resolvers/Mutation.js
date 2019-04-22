f8b2eb152a61572fdba1b443ab5c2bc376615308
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-04-22 12:49:07 -0400

    refactoring in mutations for handle several subscriptions

--- graphql-basics/src/resolvers/Mutation.js
+++ graphql-basics/src/resolvers/Mutation.js
@@ -73,29 +73,71 @@ const Mutation = {
 
     db.posts.push(post);
 
-    if (args.data.published) pobsub.publish('post', { post });
+    if (args.data.published)
+      pobsub.publish('post', {
+        post: {
+          mutation: 'CREATED',
+          data: post
+        }
+      });
 
     return post;
   },
-  deletePost(parent, args, { db }, info) {
+  deletePost(parent, args, { db, pobsub }, info) {
     const postIndex = db.posts.findIndex(post => post.id === args.id);
 
     if (postIndex === -1) throw new Error('Post not found.');
 
-    const deletedPosts = db.posts.splice(postIndex, 1);
+    const [post] = db.posts.splice(postIndex, 1);
 
     db.comments.filter(comment => comment.post !== args.id);
-    return deletedPosts[0];
+
+    if (post.published) {
+      pobsub.publish('post', {
+        post: {
+          mutation: 'DELETED',
+          data: post
+        }
+      });
+    }
+    return post;
   },
-  updatePost(parent, args, { db }, info) {
+  updatePost(parent, args, { db, pobsub }, info) {
     const { id, data } = args;
     const post = db.posts.find(post => post.id === id);
+
+    const originalPost = { ...post };
 
     if (!post) throw new Error('Post not found');
 
     if (typeof data.title === 'string') post.title = data.title;
     if (typeof data.body === 'string') post.body = data.body;
-    if (typeof data.published === 'boolean') post.published = data.published;
+    if (typeof data.published === 'boolean') {
+      post.published = data.published;
+
+      if (originalPost.published && !post.published) {
+        pobsub.publish('post', {
+          post: {
+            mutation: 'DELETED',
+            data: originalPost
+          }
+        });
+      } else if (!originalPost.published && post.published) {
+        pobsub.publish('post', {
+          post: {
+            mutation: 'CREATED',
+            data: post
+          }
+        });
+      }
+    } else if (post.publish) {
+      pobsub.publish('post', {
+        post: {
+          mutation: 'UPDATED',
+          data: post
+        }
+      });
+    }
 
     return post;
   },
