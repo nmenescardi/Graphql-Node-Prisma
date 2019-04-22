30b6ed5cdd3dfc3d0cc5f72bac56e9ba0dde8df5
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-04-22 08:37:05 -0400

    refactoring static data

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,108 +1,33 @@
 import { GraphQLServer } from 'graphql-yoga';
 import uuid from 'uuid';
 import uuidv4 from 'uuid/v4';
-
-//demo user data
-let users = [
-  {
-    id: '1',
-    name: 'Steve',
-    email: 'steve@jobs.com'
-  },
-  {
-    id: '2',
-    name: 'Scott',
-    email: 'scott@audio.com'
-  },
-  {
-    id: '3',
-    name: 'Matt',
-    email: 'matt@wp.com'
-  }
-];
-
-let posts = [
-  {
-    id: '55',
-    title:
-      'Trump, seething after Mueller report, trains his ire at ex-counsel McGahn',
-    body:
-      'Some of the report’s most derogatory scenes were attributed not only to the recollections of former White House counsel Donald McGahn and other witnesses, but also to notes kept by aides.',
-    published: false,
-    author: '1'
-  },
-  {
-    id: '56',
-    title:
-      'How a legal dispute between Mueller and Barr drove the end of the Russia probe',
-    body:
-      'The special counsel’s decision to not reach a conclusion about whether the president had obstructed justice stumped lawyers and frustrated senior Justice officials.',
-    published: true,
-    author: '2'
-  },
-  {
-    id: '57',
-    title:
-      'Mueller punted big questions to a Congress ill-equipped to deal with them',
-    body:
-      'The body is hyperpartisan and more focused on the 2020 election than issues of presidential criminality and liability.',
-    published: true,
-    author: '2'
-  }
-];
-
-let comments = [
-  {
-    id: '1',
-    text: 'comentario 1',
-    author: '1',
-    post: '55'
-  },
-  {
-    id: '2',
-    text: 'comentario 2',
-    author: '3',
-    post: '55'
-  },
-  {
-    id: '3',
-    text: 'comentario 3',
-    author: '2',
-    post: '56'
-  },
-  {
-    id: '4',
-    text: 'comentario 4',
-    author: '1',
-    post: '57'
-  }
-];
+import db from './db';
 
 // Resolvers
 const resolvers = {
   Query: {
-    users(parent, args, ctx, info) {
+    users(parent, args, { db }, info) {
       const { query } = args;
       if (!query) {
-        return users;
+        return db.users;
       } else {
-        return users.filter(user =>
+        return db.users.filter(user =>
           user.name.toLowerCase().includes(query.toLowerCase())
         );
       }
     },
-    posts(parent, args, ctx, info) {
+    posts(parent, args, { db }, info) {
       const { query } = args;
-      if (!query) return posts;
+      if (!query) return db.posts;
       else
-        return posts.filter(
+        return db.posts.filter(
           post =>
             post.title.toLowerCase().includes(query.toLowerCase()) ||
             post.body.toLowerCase().includes(query.toLowerCase())
         );
     },
-    comments(parent, args, ctx, info) {
-      return comments;
+    comments(parent, args, { db }, info) {
+      return db.comments;
     },
     me() {
       return {
@@ -121,8 +46,8 @@ const resolvers = {
     }
   },
   Mutation: {
-    createUser(parent, args, ctx, info) {
-      const emailToken = users.some(user => user.email === args.data.email);
+    createUser(parent, args, { db }, info) {
+      const emailToken = db.users.some(user => user.email === args.data.email);
 
       if (emailToken) throw new Error(`Email ${args.data.email} is taken`);
 
@@ -131,52 +56,52 @@ const resolvers = {
         ...args.data
       };
 
-      users.push(user);
+      db.users.push(user);
 
       return user;
     },
-    deleteUser(parent, args, ctx, info) {
-      const userIndex = users.findIndex(user => user.id === args.id);
+    deleteUser(parent, args, { db }, info) {
+      const userIndex = db.users.findIndex(user => user.id === args.id);
 
       if (userIndex === -1) throw new Error('user not exist');
 
-      const deletedUsers = users.splice(userIndex, 1);
+      const deletedUsers = db.users.splice(userIndex, 1);
 
-      posts = posts.filter(post => {
+      db.posts = db.posts.filter(post => {
         const match = post.author === args.id;
 
         if (match) {
-          comments = comments.filter(comment => comment.post !== post.id);
+          db.comments = db.comments.filter(comment => comment.post !== post.id);
         }
 
         return match;
       });
 
-      comments = comments.filter(comment => comment.author !== args.id);
+      db.comments = db.comments.filter(comment => comment.author !== args.id);
 
       return deletedUsers[0];
     },
-    deletePost(parent, args, ctx, info) {
-      const postIndex = posts.findIndex(post => post.id === args.id);
+    deletePost(parent, args, { db }, info) {
+      const postIndex = db.posts.findIndex(post => post.id === args.id);
 
       if (postIndex === -1) throw new Error('Post not found.');
 
-      const deletedPosts = posts.splice(postIndex, 1);
+      const deletedPosts = db.posts.splice(postIndex, 1);
 
-      comments.filter(comment => comment.post !== args.id);
+      db.comments.filter(comment => comment.post !== args.id);
       return deletedPosts[0];
     },
-    deleteComment(parent, args, ctx, info) {
-      const commentIndex = comments.findIndex(
+    deleteComment(parent, args, { db }, info) {
+      const commentIndex = db.comments.findIndex(
         comment => comment.id === args.id
       );
 
       if (commentIndex === -1) throw new Error('Comment not found.');
 
-      return comments.splice(commentIndex, 1)[0];
+      return db.comments.splice(commentIndex, 1)[0];
     },
-    createPost(parent, args, ctx, info) {
-      const userExist = users.some(user => user.id === args.data.author);
+    createPost(parent, args, { db }, info) {
+      const userExist = db.users.some(user => user.id === args.data.author);
 
       if (!userExist)
         throw new Error(`User with id ${args.data.author} does not exist.`);
@@ -186,12 +111,12 @@ const resolvers = {
         ...args.data
       };
 
-      posts.push(post);
+      db.posts.push(post);
       return post;
     },
-    createComment(parent, args, ctx, info) {
-      const userExist = users.some(user => user.id === args.data.author);
-      const postExist = posts.some(
+    createComment(parent, args, { db }, info) {
+      const userExist = db.users.some(user => user.id === args.data.author);
+      const postExist = db.posts.some(
         post => post.id === args.data.post && post.published
       );
 
@@ -208,39 +133,40 @@ const resolvers = {
         ...args.data
       };
 
-      comments.push(comment);
+      db.comments.push(comment);
       return comment;
     }
   },
   Post: {
-    author(parent, args, ctx, info) {
+    author(parent, args, { db }, info) {
       return users.find(user => user.id === parent.author);
     },
-    comments(parent, args, ctx, info) {
-      return comments.filter(comment => comment.post === parent.id);
+    comments(parent, args, { db }, info) {
+      return db.comments.filter(comment => comment.post === parent.id);
     }
   },
   User: {
-    posts(parent, args, ctx, info) {
-      return posts.filter(post => parent.id === post.author);
+    posts(parent, args, { db }, info) {
+      return db.posts.filter(post => parent.id === post.author);
     },
-    comments(parent, args, ctx, info) {
-      return comments.filter(comment => parent.id === comment.author);
+    comments(parent, args, { db }, info) {
+      return db.comments.filter(comment => parent.id === comment.author);
     }
   },
   Comment: {
-    author(parent, args, ctx, info) {
-      return users.find(user => parent.author === user.id);
+    author(parent, args, { db }, info) {
+      return db.users.find(user => parent.author === user.id);
     },
-    post(parent, args, ctx, info) {
-      return posts.find(post => post.id === parent.post);
+    post(parent, args, { db }, info) {
+      return db.posts.find(post => post.id === parent.post);
     }
   }
 };
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: { db }
 });
 
 server.start(() => console.log('server up...'));
