8258eadcc3d90ac0480920837773ddc44e0ab237
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2019-04-30 14:42:39 -0500

    add real jest test

--- graphql-prisma/tests/user.test.js
+++ graphql-prisma/tests/user.test.js
@@ -1,9 +1,46 @@
 import 'cross-fetch/polyfill';
 import ApolloBoost, { gql } from 'apollo-boost';
+import bcrypt from 'bcryptjs';
 import prisma from '../src/prisma';
 
 const client = new ApolloBoost({
   uri: 'http://localhost:4000'
+});
+
+beforeEach(async () => {
+  await prisma.mutation.deleteManyPosts();
+  await prisma.mutation.deleteManyUsers();
+  const user = await prisma.mutation.createUser({
+    data: {
+      name: 'Jen',
+      email: 'jen@example.com',
+      password: bcrypt.hashSync('4d5sa8dsa42wq')
+    }
+  });
+  await prisma.mutation.createPost({
+    data: {
+      title: 'my published post',
+      body: '',
+      published: true,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  });
+  await prisma.mutation.createPost({
+    data: {
+      title: 'my draft post',
+      body: '',
+      published: false,
+      author: {
+        connect: {
+          id: user.id
+        }
+      }
+    }
+  });
 });
 
 test('Should create a new user', async () => {
